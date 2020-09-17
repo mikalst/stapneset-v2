17148437dc4abefb08637deba994d48728de6a2d
Mikal Stapnes <mstapnes@gmail.com>
2020-09-17 14:50:36 +0100

    Trying this also

--- stapneset-web/api/fetch-weather/index.js
+++ stapneset-web/api/fetch-weather/index.js
@@ -1,14 +1,14 @@
-const AzureTables = require("@azure/data-tables");
+import { TablesSharedKeyCredential, TableClient } from "@azure/data-tables";
 
-module.exports = async function (context, req) {
+export default async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     const account = "stapnesetstorage";
     const accountKey = process.env['STAPNESET_WEATHER_STORAGE_ACCOUNT_KEY'];
     const tableName = "tasktable";
 
-    const credential = new AzureTables.TablesSharedKeyCredential(account, accountKey);
-    const client = new AzureTables.TableClient(
+    const credential = new TablesSharedKeyCredential(account, accountKey);
+    const client = new TableClient(
         `https://${account}.table.core.windows.net`,
         tableName,
         credential
