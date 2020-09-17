3ab201745206cf14f1ff551d73f3e148ae84a6d0
Mikal Stapnes <mstapnes@gmail.com>
2020-09-17 13:39:32 +0100

    Weather view now functioning
    Specify path to API
    
    Update build workflow
    
    Quickfix
    
    Fix storage account key
    
    Trying this also
    
    I did a bunch of bs
    
    Maybe fix
    
    styling

--- stapneset-web/src/components/WeatherUI.vue
+++ stapneset-web/src/components/WeatherUI.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="landingpage">
     <WeatherElement
-      v-for="[idx, temp] of weather_data.entries()"
-      v-bind:temperature="temp"
+      v-for="[idx, sixHourPeriod] of weather_data.entries()"
+      v-bind:time="sixHourPeriod.time_formatted"
+      v-bind:temperature="sixHourPeriod.air_temperature"
+      v-bind:cloud_area_fraction="sixHourPeriod.cloud_area_fraction"
+      v-bind:cloud_icon="sixHourPeriod.cloud_icon"
+      v-bind:wind_speed="sixHourPeriod.wind_speed"
       v-bind:idx="idx"
       v-bind:key="idx"
     ></WeatherElement>
@@ -20,11 +24,10 @@ export default {
   },
   data: function () { 
     return {
-      weather_data: [0, 0, 0, 0]
+      weather_data: []
     }
   },
   async created() {
-    console.log(process.env.VUE_APP_API);
     let response = await fetch(`${process.env.VUE_APP_API}`);
     let body = await response.json();
     this.weather_data = body;
