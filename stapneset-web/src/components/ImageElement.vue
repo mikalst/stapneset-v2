e46445da34bbf2d2c8932e87aab6c2ebd6739acb
Mikal Stapnes <mstapnes@gmail.com>
2020-09-21 19:23:08 +0100

    Draft image view

---
+++ stapneset-web/src/components/ImageElement.vue
@@ -0,0 +1,48 @@
+<template>
+  <transition name="fade" v-if="pageloaded">
+    <div style="width: 100%; ">
+    <img class="image" v-bind:src="image_url">
+    </div>
+  </transition>
+</template>
+
+
+<script>
+export default {
+  name: 'ImageElement',
+  props: {
+    image_url: String,
+    delay: Number,
+  },
+    data() {
+    return {
+      pageloaded: false,
+    };
+  },
+  created() {
+    setTimeout(() => { this.pageloaded = true; }, this.delay*1000);
+  }
+}
+</script>
+
+<style scoped>
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 1.0s ease-in-out;
+}
+.fade-enter, .fade-leave-active {
+  opacity: 0;
+}
+.highlight{
+  opacity: 0.5;
+  transition: opacity 0.5s ease-in-out;
+}
+.image{
+  width: 40%;
+  align-content: center;
+  z-index: 10;
+}
+</style>
+
+
+
