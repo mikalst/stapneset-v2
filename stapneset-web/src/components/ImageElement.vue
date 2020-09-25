d567fd14f0c26b62c6c4554daf2b10d9346d7d8f
Mikal Stapnes <mstapnes@gmail.com>
2020-09-25 20:07:43 +0100

    Implement the image viewer

--- stapneset-web/src/components/ImageElement.vue
+++ stapneset-web/src/components/ImageElement.vue
@@ -1,7 +1,7 @@
 <template>
   <transition name="fade" v-if="pageloaded">
-    <div style="width: 100%; ">
-    <img class="image" v-bind:src="image_url">
+    <div v-bind:class="{ blur: getBlur() }">
+    <img id="image" class="image" v-bind:src="getImageURL()">
     </div>
   </transition>
 </template>
@@ -11,13 +11,29 @@
 export default {
   name: 'ImageElement',
   props: {
-    image_url: String,
-    delay: Number,
+    image_url_hr: String,
+    image_url_lr: String,
+    blurred_array: Array,
+    idx: Number,
+    element_height: Number,
+    active_idx: Number
   },
-    data() {
+  data() {
     return {
       pageloaded: false,
     };
+  },
+  methods: {
+    getBlur: function () {
+      return !(this.active_idx ==this.idx);
+    },
+    getImageURL: function () {
+      console.log(this.image_url_hr);
+      if (this.active_idx == this.idx){
+        return this.image_url_hr;
+      }
+      return this.image_url_lr;
+    }
   },
   created() {
     setTimeout(() => { this.pageloaded = true; }, this.delay*1000);
@@ -26,7 +42,6 @@ export default {
 </script>
 
 <style scoped>
-
 .fade-enter-active, .fade-leave-active {
   transition: opacity 1.0s ease-in-out;
 }
@@ -38,9 +53,12 @@ export default {
   transition: opacity 0.5s ease-in-out;
 }
 .image{
-  width: 40%;
+  width: 50%; 
   align-content: center;
   z-index: 10;
+}
+.blur{
+  filter: blur(0px);
 }
 </style>
 
--- stapneset-web/src/components/ImagesUI.vue
+++ stapneset-web/src/components/ImagesUI.vue
@@ -1,34 +1,87 @@
 <template>
   <div id="images_app">
+    <div style="height: 20vh; "></div>
     <ImageElement
-      v-for="[idx, url] of image_urls.entries()"
-      v-bind:image_url="url"
-      v-bind:delay="idx+1"
+      v-bind:id="idx"
+      v-for="[idx, url] of image_urls_lr.entries()"
+      v-bind:image_url_hr="image_urls_hr[idx]"
+      v-bind:image_url_lr="image_urls_lr[idx]"
+      v-bind:url="url"
       v-bind:key="idx"
+      v-bind:idx="idx"
+      v-bind:active_idx="active_idx"
     ></ImageElement>
-  </div>
+      <div style="height: 60vh; "></div>
+      </div>
+    
 </template>
 
 <script>
 import ImageElement from './ImageElement.vue'
-
 
 export default {
   name: 'ImagesUI',
   components: {
     ImageElement
   },
-  data: function() 
-  {
+  data: function() {
     return {
-      image_urls: ['/image_viewer/image1.jpg', 
-                  '/image_viewer/image2.jpg',
-                  '/image_viewer/image3.jpg',
-                  '/image_viewer/image4.png', 
-                  '/image_viewer/image6.jpg',
-                  '/image_viewer/image7.jpg', 
-                  '/image_viewer/image8.png']
+      image_urls_hr: [],
+      image_urls_lr: ['/image_viewer_low_res/ender.jpg', 
+                  '/image_viewer_low_res/familie.jpg',
+                  '/image_viewer_low_res/flyfoto.jpg',
+                  '/image_viewer_low_res/gaarden.jpg', 
+                  '/image_viewer_low_res/hund.jpg',
+                  '/image_viewer_low_res/smistein.png'],
+      scrolled: false,
+      window_y_pos: 0,
+      element_height: 0,
+      active_idx: 0
     }
+  },
+  methods: {
+    changeFocused() {
+      let idx_closest_element = 0;
+      let val_closest_element = 10000;
+      for (let i = 0; i < 6; i++){
+        let elementBoundingClientRect = document.getElementById(i).getBoundingClientRect();
+        let dist = Math.abs(window.innerHeight / 2 - (elementBoundingClientRect.y + elementBoundingClientRect.height));
+        if (dist < val_closest_element){
+          idx_closest_element = i;
+          val_closest_element = dist;
+        }
+        this.active_idx = idx_closest_element
+      }
+    },
+    handleScroll () {
+      let current_window_y_pos = window.pageYOffset;
+      if (Math.abs(current_window_y_pos - this.window_y_pos) > 50){
+        this.window_y_pos = current_window_y_pos;
+        this.changeFocused();
+        // console.log(this.image_urls_hr);
+      } 
+    },
+    setImageUrls(filename_array){
+      console.log(filename_array);
+      let url_array = [];
+      let url_array_lr = [];
+        for (let i = 0; i < filename_array.length; i++){
+          url_array.push("http://localhost:7071/api/fetch-images?file="+filename_array[i]);
+          url_array_lr.push("image_viewer_low_res/"+filename_array[i]);
+        }
+      this.image_urls_hr = url_array;
+      this.image_urls_lr = url_array_lr;
+    }
+  },
+  async created () {
+    window.addEventListener('scroll', this.handleScroll);
+    this.element_height = Math.floor(window.innerHeight / 2);
+    const res = await fetch('http://localhost:7071/api/fetch-images'+`?list=true`);
+    const data = await res.json();
+    this.setImageUrls(data["filenames"]);
+  },
+  destroyed () {
+    window.removeEventListener('scroll', this.handleScroll);
   }
 }
 </script>
