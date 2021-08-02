358be6f8cb156cf9b1076e47fc1233e44d811f25
Mikal Stapnes <mstapnes@gmail.com>
2021-08-02 18:51:37 +0200

    Fix logic of weather fetching function

--- stapneset-web/api/fetch-weather/index.js
+++ stapneset-web/api/fetch-weather/index.js
@@ -3,57 +3,60 @@ const https = require('https');
 
 
 async function fetchFromYr() {
-    try {
-        let url = process.env['YR_API_ENDPOINT'];
-        let http_promise = new Promise((resolve, reject) => {
-            https.get(url, {headers: { 'User-Agent': 'test' }}, (response) => {
-                let chunks_of_data = [];
-    
-                response.on('data', (fragment) => {
-                    chunks_of_data.push(fragment);
-                });
-    
-                response.on('end', () => {
-                    let response_body = Buffer.concat(chunks_of_data);
-                    resolve(response_body.toString());
-                });
-    
-                response.on('error', (error) => {
-                    reject(error);
-                }, );
+    let url = process.env['YR_API_ENDPOINT'];
+    let http_promise = new Promise((resolve, reject) => {
+        https.get(url, {headers: { 'User-Agent': 'test' }}, (response) => {
+            let chunks_of_data = [];
+
+            response.on('data', (fragment) => {
+                chunks_of_data.push(fragment);
             });
-        });
 
-        let json = JSON.parse(await http_promise).properties.timeseries;
-        let output = [];
+            response.on('end', () => {
+                let response_body = Buffer.concat(chunks_of_data);
+                resolve(response_body.toString());
+            });
 
-        json.forEach(entry => {
-            let single = entry.data.instant.details;
-            single['RowKey'] = {'_': entry.time};
-            single['PartitionKey'] = {'_': 'taskNorway'};
-            single['timestamp'] = {'_' : entry.time};
-            output.push(single);
+            response.on('error', (error) => {
+                reject(error);
+            }, );
         });
-        return output;
+    }).catch(function(reason) {
+        throw new Error(reason);
+    });
+
+    let json = JSON.parse(await http_promise).properties.timeseries;
+
+    return json;
+}
+
+function processFromYr(yrJson, rowKey) {
+    var currentHour = rowKey;
+    var data = {
+        "current" : yrJson[0],
+        "tomorrow" : yrJson[36 - currentHour],
+        "dayafter" : yrJson[60 - currentHour]
     }
-    catch (error) {
-        console.log(error);
-    }
+    return {
+        "data": JSON.stringify(data),
+        "RowKey": rowKey,
+        "PartitionKey": rowKey
+    };
 }
 
 async function store(tableSvc, tableName, data) {
     let promise = new Promise((resolve, reject) => {
-        data.forEach(entry => {
-            tableSvc.insertOrReplaceEntity(tableName, entry, {echoContent: true}, function (error, result, response) {
-                if(!error) {
-                    // Insertion completed
-                }
-                else{
-                    reject("Did not finish");
-                }
-            });
+        tableSvc.insertOrReplaceEntity(tableName, data, {echoContent: true}, function (error, result, response) {
+            if(!error) {
+                // Insertion completed
+                resolve("Finished writing to table");
+            }
+            else{
+                reject(error);
+            }
         });
-        resolve("Finished writing to table");
+    }).catch(function(reason) {
+        console.log(reason);
     });
     await promise;
 }
@@ -64,58 +67,64 @@ module.exports = async function (context, req) {
     const connectionString = process.env['STORAGE_CONNECTION_STRING'];
     const tableName = process.env['STORAGE_TABLE_NAME'];
 
+    var roundedDate = new Date().getHours();
+    const rowKey = roundedDate.toString();
+
     try{
         var tableSvc = azure.createTableService(connectionString);
 
-        let weather_data = [];
+        var weatherData = {};
 
-        let date = new Date(Date.now() + 60*60000);
-        date.setMinutes(0);
-        date.setSeconds(0);
-        date.setMilliseconds(0);
-        
-        for (i = 0; i < 3; i++) {
-            let rowKey = date.toISOString().slice(0, -5)+"Z";
-            
-            let promise = new Promise(function(resolve, reject) {
-                tableSvc.retrieveEntity(tableName, 'taskNorway', rowKey, async function(error, result, response) {
+        let promise = new Promise(function(resolve, reject) {
+            tableSvc.retrieveEntity(
+                tableName,
+                rowKey,
+                rowKey,
+                async function(error, result, response) {
                     if(!error) {
                         //Query was successful
-                        weather_data.push(result);
+                        weatherData = result;
+                    
+                        console.log("Successfully fetched data from storage.");
                         resolve("Success");
                     }
                     else {
+                        console.debug("[DEBUG] Failed fetching from storage", error);
+
                         //Fetch data from yr
-                        data = await fetchFromYr(rowKey);
+                        rawWeatherData = await fetchFromYr();
+                        weatherData = processFromYr(rawWeatherData, rowKey);
 
                         //Store data to Azure
-                        await store(tableSvc, tableName, data);
+                        await store(tableSvc, tableName, weatherData);
 
-                        //Push to output
-                        tableSvc.retrieveEntity(tableName, 'taskNorway', rowKey, async function(error, result, response) {
-                            if(!error) {
-                                //Query was successful
-                                weather_data.push(result);
-                                resolve("Success");
-                            }
-                            else {
-                                reject("Failure!");
-                            }
-                        });
+                        console.log("[DEBUG] Successfully fetched and stored data from Yr.");
+                        resolve("Success");
                     }
-                });
-            });
-            await promise;
-            date.setHours(date.getHours() + 24);
-        }
+                }
+            );
+        }).catch(function(reason) {
+            throw new Error(reason);
+        });
+
+        await promise;
+
+        console.log(weatherData);
+        
+        parsedWeatherData = {
+            "data": JSON.parse(weatherData.data['_']),
+            "RowKey": weatherData.RowKey,
+            "PartitionKey": weatherData.PartitionKey
+        };
+
         context.res = {
-            body: JSON.stringify({"data": weather_data})
+            body: JSON.stringify(parsedWeatherData)
         };
     }
     catch (error) {
-        console.log(error);
+        console.error(error);
         context.res = {
-            body: JSON.stringify({"data": [null, null, null]})
+            body: JSON.stringify({"data": {"current": null, "tomorrow": null, "dayafter": null}})
         };
     }
 }
--- stapneset-web/src/components/WeatherUI.vue
+++ stapneset-web/src/components/WeatherUI.vue
@@ -2,10 +2,10 @@
   <div id="weather_app">
     <WeatherElement
       v-for="[idx, sixHourPeriod] of weather_data.entries()"
-      v-bind:time="new Date(sixHourPeriod.RowKey['_'])"
-      v-bind:temperature="sixHourPeriod.air_temperature['_']"
-      v-bind:cloud_area_fraction="sixHourPeriod.cloud_area_fraction['_']"
-      v-bind:wind_speed="sixHourPeriod.wind_speed['_']"
+      v-bind:time="new Date(Date.parse(sixHourPeriod.time))"
+      v-bind:temperature="sixHourPeriod.details.air_temperature"
+      v-bind:cloud_area_fraction="sixHourPeriod.details.cloud_area_fraction"
+      v-bind:wind_speed="sixHourPeriod.details.wind_speed"
       v-bind:idx="idx"
       v-bind:key="idx"
     ></WeatherElement>
@@ -29,7 +29,20 @@ export default {
   async created() {
     let response = await fetch(process.env.VUE_APP_API_PATH+"fetch-weather");
     let body = await response.json();
-    this.weather_data = body["data"];
+    this.weather_data = [
+      {
+        "time": body.data.current.time,
+        "details": body.data.current.data.instant.details
+      },
+      {
+        "time": body.data.tomorrow.time,
+        "details": body.data.tomorrow.data.instant.details
+      },
+      {
+        "time": body.data.dayafter.time,
+        "details": body.data.dayafter.data.instant.details
+      }];
+    console.debug(this.weather_data);
   }
 }
 </script>
