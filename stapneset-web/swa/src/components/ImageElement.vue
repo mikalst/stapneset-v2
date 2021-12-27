12e313c8cc954cc231a844f5037feaa488c3fbd9
Mikal Stapnes <mikal.stapnes@devimdi.no>
2021-12-27 16:18:08 +0100

    Styling

--- stapneset-web/swa/src/components/ImageElement.vue
+++ stapneset-web/swa/src/components/ImageElement.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="container image py-2">
-    <b-row>
-      <b-card>
-      <b-img left rounded fluid-grow v-bind:src="url" class="py-6" />
-        {{ caption }}
-      </b-card>
-    </b-row>
+  <div class="container image py-2 text-left">
+    <!-- <b-img left rounded fluid-grow v-bind:src="url" class="py-6" /> -->
+    <b-card :img-src="url" img-top >
+      <b-card-text>
+        {{ caption }} 
+      </b-card-text>
+    </b-card>
   </div>
 </template>
 
--- stapneset-web/swa/src/components/WeatherElement.vue
+++ stapneset-web/swa/src/components/WeatherElement.vue
@@ -1,10 +1,10 @@
 <template>
-  <b-col variant="light" right class="nonbg wide">
+  <b-col variant="light" right class="nonbg wide text-center">
     <b-icon v-if="cloud_icon=='wb_cloudy'" variant="light" icon="cloud-haze" size="lg" class="icon">Cloudy</b-icon>
     <b-icon v-if="cloud_icon=='wb_sunny'" variant="light"  icon="sun-fill" size="lg" class="icon">Sunny</b-icon>
     <b-icon v-if="cloud_icon=='filter_drama'"  variant="light" icon="cloud-sun" size="lg" class="icon">Partly cloudy</b-icon>
   </b-col> 
-  <b-col variant="light" class="nonbg wide">
+  <b-col variant="light" class="nonbg wide text-center">
     <b-row variant="light">
       <b-badge variant="light">
         {{ days[time.getDay()] + "\n Kl." + time.getHours() }}
@@ -74,19 +74,8 @@ export default {
 @import "https://fonts.googleapis.com/icon?family=Material+Icons";
 
 @media screen and (max-width: 600px) {
-  .base {
-    z-index: 1;
-  }
   .wide {
     flex: 1 1 100%;
-  }
-}
-@media screen and (min-width: 601px) {
-  .base {
-    z-index: 1;
-  }
-  .time {
-    text-align: center;
   }
 }
 .icon {
