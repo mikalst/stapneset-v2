e8234cbc41bb33f161438673ba797ec9f01d2e3d
Mikal Stapnes <mstapnes@gmail.com>
2022-06-19 10:05:19 +0200

    Remove bootstrap

--- stapneset-web/swa/src/methods/processCaption.js
+++ stapneset-web/swa/src/methods/processCaption.js
@@ -3,23 +3,17 @@ function processCaption(input) {
 
     var caption = input.caption;
 
-    // Parse index
-    var indexRegex = /\[(?<index>[\d]*)\]/m;
-    let indexMatch = caption?.match(indexRegex);
-    if (indexMatch) {
-        console.debug("[DEBUG] Found index", indexMatch);
-        output.index = parseInt(indexMatch.groups.index) ?? 999;
-        caption = 
-            caption?.substring(0, indexMatch.index) + 
-            caption?.substring(indexMatch.index + indexMatch[0].length, caption.length);
-    }
-
-    // Parse area
-    var tagRegex = /#(?<area>\S+)/m;
-    let match = caption?.match(tagRegex);
+    var regex = /STAP;(?<area>\S+);(?<index>[\d]*);(?<tags>[\S\ ,]+)/m;
+    let match = caption?.match(regex);
     if (match) {
-        console.debug("[DEBUG] Found area", match);
+        // Parse index
+        console.debug("[DEBUG] Found match", match);
+        output.index = parseInt(match.groups.index) ?? 999;
+
         output.area = match.groups.area.toLowerCase();
+
+        output.tags = match.groups.tags.split(',').map(tag => tag.trim());
+
         caption = 
             caption?.substring(0, match.index) + 
             caption?.substring(match.index + match[0].length, caption.length);
